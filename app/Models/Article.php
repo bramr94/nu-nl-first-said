162002e86de8d353efad40699664f712099dde5f
Bram Raaijmakers <b.raaijmakers@bluedragon.nl>
2020-12-20 17:34:01 +0100

    Add fillable fields to the Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Article
+ *
+ * @author Bram Raaijmakers
+ *
+ * @package App\Models
+ */
 class Article extends Model
 {
-    use HasFactory;
+    /**
+     * @var array
+     */
+    protected $fillable = ['article_id', 'url', 'title', 'content'];
 }
