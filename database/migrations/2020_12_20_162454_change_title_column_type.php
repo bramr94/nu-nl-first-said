191c4694f9d5d467312b7563576a70e814875f23
Bram Raaijmakers <b.raaijmakers@bluedragon.nl>
2020-12-20 19:01:06 +0100

    Change file and class name.

--- database/migrations/2020_12_20_162454_change_title_column_type.php
+++ database/migrations/2020_12_20_162454_change_title_column_type.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeContentColumnType extends Migration
+class ChangeTitleColumnType extends Migration
 {
     /**
      * Run the migrations.
