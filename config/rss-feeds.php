ab4ce91c6500d3c792264bdc247164cd0619d767
Bram Raaijmakers <b.raaijmakers@bluedragon.nl>
2021-03-06 12:37:56 +0100

    Add config with the RSS feeds

---
+++ config/rss-feeds.php
@@ -0,0 +1,16 @@
+<?php
+
+// The different RSS feeds from nu.nl - https://www.nu.nl/rss.html
+return [
+    'urls' => [
+        'https://www.nu.nl/rss/Algemeen',
+        'https://www.nu.nl/rss/Economie',
+        'https://www.nu.nl/rss/Sport',
+        'https://www.nu.nl/rss/Achterklap',
+        'https://www.nu.nl/rss/Opmerkelijk',
+        'https://www.nu.nl/rss/Film',
+        'https://www.nu.nl/rss/Wetenschap',
+        'https://www.nu.nl/rss/Tech',
+        'https://www.nu.nl/rss/Gezondheid'
+    ]
+];
